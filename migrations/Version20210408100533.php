45cd200dfb0da6948d02376844e513420ed9c25f
mlraleader <mouwenlegan@gmail.com>
2021-04-08 11:16:31 +0100

    Mise à jour de projet avec composer update

---
+++ migrations/Version20210408100533.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210408100533 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, phone_number VARCHAR(255) NOT NULL, phone_number2 VARCHAR(255) DEFAULT NULL, mail_contact2 VARCHAR(255) DEFAULT NULL, social_objet LONGTEXT NOT NULL, principal_activite VARCHAR(255) NOT NULL, denomination VARCHAR(255) NOT NULL, sigle VARCHAR(255) DEFAULT NULL, numero_recepisse VARCHAR(255) NOT NULL, date_creation DATE NOT NULL, adresse VARCHAR(255) NOT NULL, site_internet VARCHAR(255) DEFAULT NULL, lien_facebook VARCHAR(255) DEFAULT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+    }
+}
