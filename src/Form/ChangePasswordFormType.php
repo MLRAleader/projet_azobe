f36f193340d9205cd05fc55ae80113a6d0cd6df2
mlraleader <mouwenlegan@gmail.com>
2021-05-27 12:07:21 +0100

    Stylisation du formulaire d'inscription

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -20,7 +20,7 @@ class ChangePasswordFormType extends AbstractType
                 'first_options' => [
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please enter a password',
+                            'message' => 'Entrez un mot de passe',
                         ]),
                         new Length([
                             'min' => 6,
@@ -29,12 +29,12 @@ class ChangePasswordFormType extends AbstractType
                             'max' => 4096,
                         ]),
                     ],
-                    'label' => 'New password',
+                    'label' => 'Nouveau mot de passe',
                 ],
                 'second_options' => [
-                    'label' => 'Repeat Password',
+                    'label' => 'Répétez le mot de passe',
                 ],
-                'invalid_message' => 'The password fields must match.',
+                'invalid_message' => 'Les deux mot de passe doivent correspondre.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -55,7 +56,7 @@ class RegistrationFormType extends AbstractType
             ->add('province', ProvinceFormType::class)
             ->add('sigle',TextType::class,array('attr'=>array('placeholder'=>'Une abréviation de votre organisation')))
             ->add('numero_recepisse',TextType::class,array('attr'=>array('placeholder'=>'Le numéro de votre récépissé')))
-            ->add('date_creation')
+            ->add('date_creation',DateType::class,array('label'=>'Date de création'))
             ->add('adresse',TextType::class,array('attr'=>array('placeholder'=>'La ville de votre siège')))
             ->add('site_internet',TextType::class,array('attr'=>array('placeholder'=>'Le lien de votre site internet')))
             ->add('lien_facebook',TextType::class,array('attr'=>array('placeholder'=>'Le lien facebook de votre page')))
